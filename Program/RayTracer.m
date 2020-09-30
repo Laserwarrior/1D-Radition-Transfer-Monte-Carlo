6e3503b112785eac28f7f61850ecf09fb99e4301
Laserwarrior <42187562+Laserwarrior@users.noreply.github.com>
2020-09-29 23:27:03 -0500

    Delete RayTracer.m

--- Program/RayTracer.m
+++
@@ -1,9 +0,0 @@
-function [Travel_Distance_magnitude, Position] = RayTracer(Spatial_Material_Parameter, Current_Material_Parameter, Position, Angle)
-
-Total_CrossSection = Current_Material_Parameter.Sigma_t;
-Travel_Distance_magnitude = -1/Total_CrossSection*log(rand);
-Travel_Distance_Z_direction = Travel_Distance_magnitude * Angle;
-Position = Position + Travel_Distance_Z_direction;
-
-
-end
